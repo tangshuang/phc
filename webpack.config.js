d620d63ef830548ce8851f5b23dac7bcae9d78db
frustigor <frustigor@frustigordeMac-mini.local>
2024-01-18 22:19:27 +0800

    chore: 格式化

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,37 @@ const path = require('path');
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'none';
 const devtool = mode === 'production' ? 'source-map' : undefined;
 const alias = {
-  'ts-fns': path.resolve(__dirname, 'node_modules/ts-fns/es'),
+    'ts-fns': path.resolve(__dirname, 'node_modules/ts-fns/es'),
 };
 const defines = new DefinePlugin({
-  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
 });
 const fallback = {
 };
 const optimization = {
-  usedExports: true,
-  sideEffects: true,
+    usedExports: true,
+    sideEffects: true,
 };
 
 module.exports = [
-  {
-    mode,
-    devtool,
-    target: 'web',
-    entry: path.resolve(__dirname, 'es/index.js'),
-    output: {
-      path: __dirname,
-      filename: 'index.js',
-      library: 'phc',
-      libraryTarget: 'umd',
+    {
+        mode,
+        devtool,
+        target: 'web',
+        entry: path.resolve(__dirname, 'es/index.js'),
+        output: {
+            path: __dirname,
+            filename: 'index.js',
+            library: 'phc',
+            libraryTarget: 'umd',
+        },
+        resolve: {
+            alias,
+            fallback,
+        },
+        plugins: [
+            defines,
+        ],
+        optimization,
     },
-    resolve: {
-      alias,
-      fallback,
-    },
-    plugins: [
-      defines,
-    ],
-    optimization,
-  },
 ];
